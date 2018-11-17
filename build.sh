3a471b65bd6581a685672961d813a7d8a3f07c6d
Steven Showers <steven.showers@docker.com>
2018-11-16 22:58:57 -0500

    changed output dir

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /root/build/out
-/root/build/package-release.sh master /root/build/out --no-package
-chown -R $UID:$GID /root/build/out
+mkdir -p /root/build/DXVK-MASTER
+/root/build/package-release.sh master /root/build/DXVK-MASTER --no-package
+chown -R $UID:$GID /root/build/DXVK-MASTER
